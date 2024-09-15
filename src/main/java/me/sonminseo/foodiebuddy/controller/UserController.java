a58e6292beb8a598f23b2324aa8831c078677758
minseoooooo <oesnimnos@gmail.com>
2024-09-16 08:44:21 +0900

    feat: POST /user/signup 유저 추가 기능(임시용) 구현

--- src/main/java/me/sonminseo/foodiebuddy/controller/UserController.java
+++ src/main/java/me/sonminseo/foodiebuddy/controller/UserController.java
@@ -3,7 +3,10 @@ package me.sonminseo.foodiebuddy.controller;
 import lombok.RequiredArgsConstructor;
 import me.sonminseo.foodiebuddy.dto.UserDr1RequestDto;
 import me.sonminseo.foodiebuddy.dto.UserDr1ResponseDto;
+import me.sonminseo.foodiebuddy.dto.UserSignUpRequestDto;
+import me.sonminseo.foodiebuddy.dto.UserSignUpResponseDto;
 import me.sonminseo.foodiebuddy.entity.Ingredients;
+import me.sonminseo.foodiebuddy.entity.User;
 import me.sonminseo.foodiebuddy.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,11 +22,20 @@ public class UserController {
 
     private final UserService userService;
 
+    // 계정 생성
+
+    @PostMapping("/signup")
+    @ResponseStatus(value = HttpStatus.CREATED)
+    public UserSignUpResponseDto userSignUpResponseDto(@RequestBody final UserSignUpRequestDto userSignUpRequestDto) {
+        Long userId = userService.signUp(userSignUpRequestDto);
+        User findUser = userService.findUserById(userId);
+        return UserSignUpResponseDto.from(findUser);
+    }
+
     // 식이제한 입력 step1
     @PostMapping("/dr1")
     @ResponseStatus(value = HttpStatus.OK)
     public UserDr1ResponseDto userDr1ResponseDto(@RequestBody final UserDr1RequestDto userDr1RequestDto) {
-
         Ingredients ingredients = userService.receivedDr1(userDr1RequestDto);
 
 //        User findUser = userService.findUserById(userId);
