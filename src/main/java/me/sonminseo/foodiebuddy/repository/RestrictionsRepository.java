aa08b4b52ded4c2ceeea8a43b3d225e58d2abccc
minseoooooo <oesnimnos@gmail.com>
2024-09-16 08:18:27 +0900

    fix: restriction 조회 안되는 에러 수정중

--- src/main/java/me/sonminseo/foodiebuddy/repository/RestrictionsRepository.java
+++ src/main/java/me/sonminseo/foodiebuddy/repository/RestrictionsRepository.java
@@ -2,11 +2,12 @@ package me.sonminseo.foodiebuddy.repository;
 
 
 import me.sonminseo.foodiebuddy.entity.Restrictions;
+import me.sonminseo.foodiebuddy.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RestrictionsRepository extends JpaRepository<Restrictions, Long> {
 
-    Restrictions findByUserUserId(Long userId);
+    Restrictions findByUser(User user);
 
 
 }
