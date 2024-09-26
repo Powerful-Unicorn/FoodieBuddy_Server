2d04084eaf3ed6ec37b80e71fe81e00137f50cdf
minseoooooo <oesnimnos@gmail.com>
2024-09-26 09:54:27 +0900

    chore: 자동 추가된 기본 로그인 화면 제거

--- src/main/java/me/sonminseo/foodiebuddy/FoodieBuddyServerApplication.java
+++ src/main/java/me/sonminseo/foodiebuddy/FoodieBuddyServerApplication.java
@@ -2,12 +2,13 @@ package me.sonminseo.foodiebuddy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class FoodieBuddyServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FoodieBuddyServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FoodieBuddyServerApplication.class, args);
+    }
 
 }
